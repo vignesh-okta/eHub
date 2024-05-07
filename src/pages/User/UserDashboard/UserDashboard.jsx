7db434e0c0e747e2a889f4dbe4bf433246c8cb9b
vignesh-okta <vignesh.aln@gmail.com>
2024-05-06 22:03:02 -0700

    dashboard updates

--- src/pages/User/UserDashboard/UserDashboard.jsx
+++ src/pages/User/UserDashboard/UserDashboard.jsx
@@ -3,7 +3,17 @@ import SidebarMenu from "../../../components/component/SidebarMenu/SidebarMenu";
 import "./UserDashboard.scss";
 
 function UserDashboard() {
-  const name = "Padma";
+  function getName(){
+    const token = JSON.parse(atob(localStorage.getItem('token').split('.')[1]))
+    if (token['iss']=="https://accounts.google.com"){
+      return token['name']
+    }
+
+    else {
+      return token['firstName'] + " " + token['lastName']
+    }
+  }
+  const name = getName();
   return (
     <div className="dashboard">
       <SidebarMenu name={name} />
